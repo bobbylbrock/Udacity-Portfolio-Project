5a12c151cb2be038ef252c26b392f027c5b99c18
Bobby Brock <bbrock@freshhospitality.net>
2016-01-24 22:27:39 -0600

    Updated CSS to include max-width, added some JQuery, and fixed styles that were in the HTML

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+$('img').hide();
+$('img').each( function(){
+    $(this).on('load', function () {
+        $(this).fadeIn(1000);
+    });
+});
+
+$("h1, h2, p")
+  .css('opacity', 0)
+  .slideDown('slow')
+  .animate(
+    { opacity: 1 },
+    { queue: false, duration: 'slow' }
+  );
